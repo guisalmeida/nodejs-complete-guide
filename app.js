1bffc11b920404f60e0e9e45de76a0ea3b874ede
guisalmeida <guisalmeida.dev@gmail.com>
2025-02-13 16:18:00 -0300

    refactor: app.js

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const express = require('express');
 const path = require('path');
+const bodyParser = require('body-parser');
 
+const errorController = require('./src/controllers/errorController');
 const { adminRoutes } = require('./src/routes/admin');
+const { shopRoutes } = require('./src/routes/shop');
 const { mongoConnect } = require('./src/utils/database');
 
 const app = express();
@@ -10,6 +13,12 @@ app.set('view engine', 'ejs');
 app.set('views', path.join(__dirname, 'src', 'views'));
 
 app.use('/admin', adminRoutes);
+app.use(shopRoutes);
+
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(express.static(path.join(__dirname, 'public')));
+
+app.use(errorController.getError);
 
 mongoConnect(() => {
   app.listen(3000);
