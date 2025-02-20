944b1a2d4b8274aadb12d6419a2777860bc9e47e
guisalmeida <guisalmeida.dev@gmail.com>
2025-02-20 15:15:54 -0300

    refactor: product model

--- src/models/productModel.js
+++ src/models/productModel.js
@@ -16,78 +16,13 @@ const productSchema = new Schema({
   imageUrl: {
     type: String,
     required: true
+  },
+  userId: {
+    type: Schema.Types.ObjectId,
+    ref: 'user',
+    required: true
   }
 });
-
-// const { ObjectId } = require('mongodb');
-
-// class ProductModel {
-//   constructor({ title, price, description, imageUrl, productId, userId }) {
-//     this.title = title;
-//     this.price = price;
-//     this.description = description;
-//     this.imageUrl = imageUrl;
-//     this._id = productId ? new ObjectId(productId) : null;
-//     this.userId = userId;
-//   }
-
-//   save() {
-//     const db = getDb();
-//     let dbOperation;
-
-//     if (this._id) {
-//       dbOperation = db.collection('products').updateOne({ _id: this._id }, { $set: this });
-//     } else {
-//       dbOperation = db.collection('products').insertOne(this);
-//     }
-
-//     return dbOperation
-//       .then(result => {
-//         console.log(result);
-//       })
-//       .catch(err => {
-//         console.log("error to save:", err);
-//       });
-//   }
-
-//   static fetchAll() {
-//     const db = getDb();
-//     return db
-//       .collection('products')
-//       .find()
-//       .toArray()
-//       .then(products => {
-//         console.log(products);
-//         return products;
-//       })
-//       .catch(err => {
-//         console.log(err);
-//       });
-//   }
-
-//   static findById(prodId) {
-//     const db = getDb();
-//     return db
-//       .collection('products')
-//       .findOne({ _id: new ObjectId(prodId) })
-//       .then(product => product)
-//       .catch(err => console.log(err));
-//   }
-
-//   static delete(prodId) {
-//     const db = getDb();
-//     return db
-//       .collection('products')
-//       .deleteOne({ _id: new ObjectId(prodId) })
-//       .then(result => {
-//         console.log(result);
-//         return result;
-//       })
-//       .catch(err => {
-//         console.log(err);
-//       });
-//   }
-// }
 
 const ProductModel = model('product', productSchema);
 
